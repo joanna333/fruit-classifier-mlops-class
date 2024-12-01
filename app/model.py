931453765398f12e3e6bde1c340a75dc2ed03463
joanna333 <joanna333@users.noreply.github.com>
2024-12-01 13:41:53 +0000

    Implement model loading and artifact downloading in model.py

--- app/model.py
+++ app/model.py
@@ -1,27 +1,58 @@
 import os
+from pathlib import Path
 import wandb
 from loadotenv import load_env
+import torch
+from torchvision.models import resnet18, ResNet
+from torch import nn
+from torchvision import transforms
 
 
 load_env()
 wandb_api_key = os.environ.get('WANDB_API_KEY')
-print(wandb_api_key)
 
 MODELS_DIR = 'models'
+MODEL_FILE_NAME = 'best_model.pth'
 
 os.makedirs(MODELS_DIR, exist_ok=True)
 
 def download_artifact():
-    #assert 'WANDB_API_KEY' in os.environ, 'Please enter the wandb API key'
-
     wandb_org = os.environ.get('WANDB_ORG')
     wandb_project = os.environ.get('WANDB_PROJECT')
     wandb_model_name = os.environ.get('WANDB_MODEL_NAME')
     wandb_model_version = os.environ.get('WANDB_MODEL_VERSION')
 
+
     artifact_path = f'{wandb_org}/{wandb_project}/{wandb_model_name}:{wandb_model_version}'
-    artifact_path = artifact_path.replace("'", "")
-    print(f"Artifact Path: {artifact_path}")
+
+    wandb.login()
+    artifact = wandb.Api().artifact(artifact_path, type='model')
+    artifact.download(root=MODELS_DIR)
     
 
-download_artifact() 
+def get_raw_model() -> ResNet:
+    """ Here we create a model with the same architecture as the one used in Kaggle."""
+    architecture = resnet18(weights=None)
+    # Change the model architecture to match the one used in Kaggle
+    architecture.fc = nn.Sequential(
+        nn.Linear(512, 512),
+        nn.ReLU(),
+        nn.Linear(512, 6)
+    )
+    return architecture
+
+def load_model() -> ResNet:
+    """This returns the model with the weights from the best run saved on wandb."""
+    download_artifact()
+    model = get_raw_model()
+    # Get the trained model weights
+    model_state_dict_path = Path(MODELS_DIR) / MODEL_FILE_NAME
+    model_state_dict = torch.load(model_state_dict_path, map_location='cpu')
+    # Assign the trained model weights to the model
+    model.load_state_dict(model_state_dict, strict=True)
+    # Turn off BatchNorm and Dropout
+    model.eval()
+    return model
+
+
+print(load_model())
